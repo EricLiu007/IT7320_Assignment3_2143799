961cf6e4e56ba3d6c48e2e6fc81a09b75653159c
Zhiyang.Liu <Zhiyang.Liu@B204-01.soit.local>
2017-09-26 18:09:48 +1300

    Mockito Activity

---
+++ src/test/java/IT7320/Assignment3/TestCalcImplementation.java
@@ -0,0 +1,54 @@
+package IT7320.Assignment3;
+
+import junit.framework.TestCase;
+import static org.mockito.Mockito.*;
+
+public class TestCalcImplementation extends TestCase {
+
+	CalcImplementation obj;
+	int x, y;
+	
+	protected void setUp() throws Exception {
+		
+		obj = new CalcImplementation();
+		x = 12;
+		y = 4;
+
+		CalcInterface mockObj = mock(CalcInterface.class);
+		when(mockObj.add(x, y)).thenReturn(x + y);
+		when(mockObj.subtract(x, y)).thenReturn(x - y);
+		when(mockObj.multiply(x, y)).thenReturn(x * y);
+		when(mockObj.divide(x, y)).thenReturn((double) (x / y));
+		obj.setIntObj(mockObj);
+		
+		
+	}
+
+	protected void tearDown() throws Exception {
+		
+		obj = null;
+		x = 0;
+		y = 0;
+	}
+
+	public void testAddTwoNums() {
+	
+		assertEquals(16, obj.addTwoNums(x, y));
+	}
+
+	public void testSubtractTwoNums() {
+		
+		assertEquals(8, obj.subtractTwoNums(x, y));
+	}
+
+	public void testMultiplyTwoNums() {
+		
+		assertEquals(48, obj.multiplyTwoNums(x, y));
+	}
+
+	public void testDivideTwoNums() {
+		
+		assertEquals(3.0, obj.divideTwoNums(x, y));
+	}
+
+}
